953927fedabd0a8cc3679af2caf4ee65fa98e165
Jason Youngberg <jasondyoungberg@gmail.com>
2024-11-13 19:18:04 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+#![no_std]
+
+/// This macro will tigger a linker error if it can't be optimized out
+/// ```rust
+/// use unreachable_checked::unreachable_checked;
+///
+/// let a = [1, 2, 3, 4];
+/// for i in 0..4 {
+///    println!("{}", a.get(i).unwrap_or_else(|| unreachable_checked!()))
+/// }
+/// ```
+#[macro_export]
+macro_rules! unreachable_checked {
+    () => {{
+        extern "Rust" {
+            #[link_name = concat!(
+                                    "\n\n\n",
+                                    "ERROR: unreachable_checked!() not optimized out at ",
+                                    file!(),
+                                    ":",
+                                    line!(),
+                                    ":",
+                                    column!(),
+                                    "\n\n\n")]
+            fn check() -> !;
+        }
+        unsafe { check() };
+    }};
+}
